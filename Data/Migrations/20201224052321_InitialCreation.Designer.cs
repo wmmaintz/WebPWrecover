16ca4c378cd8fa69cf88aa1f18b4919bab7ef498
Bill Maintz <bill@allbusinesscomputerservices.com>
2020-12-24 00:40:17 -0600

    Added content to the secrets.json file in %APPDATA%\Microsoft\UserSecrets\<UserSecretsId>\secrets.json

--- Data/Migrations/20201224052321_InitialCreation.Designer.cs
+++ Data/Migrations/20201224052321_InitialCreation.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebPWrecover.Data;
 
 namespace WebPWrecover.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201224052321_InitialCreation")]
+    partial class InitialCreation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
